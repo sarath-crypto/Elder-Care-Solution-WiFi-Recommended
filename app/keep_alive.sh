9b72422d1dc0f2e1c4cce561eaa64263a9cec048
sarath-crypto <76784482+sarath-crypto@users.noreply.github.com>
2024-11-20 09:06:42 +0530

    Delete app directory

--- app/keep_alive.sh
+++
@@ -1,8 +0,0 @@
-while true
-do 
-	cmd=$(pgrep ecsysapp)
-	if [ -z "$cmd" ]; then
-		/home/ecsys/app/ecsysapp
-	fi
-	sleep 30
-done
--- app/sta2.sh
+++
@@ -1,3 +0,0 @@
-sudo nmcli connection add type 802-11-wireless con-name sarath_nivas ssid sarath_nivas 802-11-wireless-security.key-mgmt WPA-PSK 802-11-wireless-security.psk land1234
-sudo nmcli con mod sarath_nivas connection.autoconnect true
-sudo nmcli connection up sarath_nivas
