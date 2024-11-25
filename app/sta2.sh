e0f59a89e9beb1f5293749ed04efeb23b914cfd6
sarath-crypto <76784482+sarath-crypto@users.noreply.github.com>
2024-11-25 09:14:50 +0530

    Delete app directory

--- app/sta2.sh
+++
@@ -1,3 +0,0 @@
-sudo nmcli connection add type 802-11-wireless con-name <connection name> ssid myssid 802-11-wireless-security.key-mgmt WPA-PSK 802-11-wireless-security.psk myssid
-sudo nmcli con mod <connection name> connection.autoconnect true
-sudo nmcli connection up <connection name>
